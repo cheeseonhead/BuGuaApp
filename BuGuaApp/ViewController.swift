f97a1e35aca7b647fa1f74cf76de9e7c03c7323f
Jeffrey Wu <jwu@touchbistro.com>
2018-08-27 22:04:27 -0400

    Initial Commit

---
+++ BuGuaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-08-27.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
