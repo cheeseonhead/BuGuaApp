0e9969603f7e70e581e7f7170d60fe2b47af8c13
Jeffrey Wu <jwu@touchbistro.com>
2018-09-03 21:22:18 -0400

    Added files for guaXiangViewController

---
+++ BuGuaApp/GuaXiangViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GuaXiangViewController.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-09-03.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GuaXiangViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
