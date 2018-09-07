f72b1546dfbf76b8d6b6450ac5954885b14bc1cd
Jeff Woo <cheeseonhead@gmail.com>
2018-09-06 23:05:01 -0400

    Now able to input three numbers and get a guaXiang

--- BuGuaApp/InputViewController.swift
+++ BuGuaApp/InputViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import RxSwift
 
 class InputViewController: UIViewController {
 
     // MARK: - Views
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var finishButton: UIButton!
-    @IBOutlet weak var firstField: UITextField!
-    @IBOutlet weak var secondField: UITextField!
-    @IBOutlet weak var thirdField: UITextField!
+    @IBOutlet weak var field1: UITextField!
+    @IBOutlet weak var field2: UITextField!
+    @IBOutlet weak var field3: UITextField!
     @IBOutlet var fields: [UITextField]!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    // MARK: - Rx
+    let bag = DisposeBag()
     
     // MARK: - Private properties
     private let viewModel: InputViewModel
@@ -34,24 +39,60 @@ class InputViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupDelegate()
         styling()
+        bindings()
     }
 }
 
 private extension InputViewController {
+    func setupDelegate() {
+        let numberDelegate = NumericTextFieldDelegate()
+        let autoNextDelegate = AutoNextTextFieldDelegate(textFields: [field1, field2, field3])
+        textFieldDelegate = ComposedTextFieldDelegate(delegates: [numberDelegate, autoNextDelegate], &&)
+        
+        autoNextDelegate.finishRelay.withLatestFrom(Observable.combineLatest(field1.rx.text, field2.rx.text, field3.rx.text))
+            .map { str1, str2, str3 in
+                return InputViewModel.Input<String?>(field1: str1, field2: str2, field3: str3)
+            }.bind(to: viewModel.inputRelay)
+            .disposed(by: bag)
+    }
+    
     func styling() {
         titleLabel.textColor = .spaceGrey
         titleLabel.font = .title2
         finishButton.titleLabel?.font = .title1
         
-        let numberDelegate = NumericTextFieldDelegate()
-        let autoNextDelegate = AutoNextTextFieldDelegate(textFields: [firstField, secondField, thirdField])
-        textFieldDelegate = ComposedTextFieldDelegate(delegates: [numberDelegate, autoNextDelegate], &&)
+        errorLabel.textColor = .scarlet
+        errorLabel.font = .title1
+        errorLabel.isHidden = true
+        
+        field1.placeholder = "內掛數"
+        field2.placeholder = "外掛數"
+        field3.placeholder = "動爻數"
         
         fields.forEach { field in
             field.font = .headline
             field.delegate = textFieldDelegate
         }
+    }
+    
+    func bindings() {
+        viewModel.resultRelay.map { result in
+            switch result {
+            case .success: return true
+            case .error: return false
+            }
+        }.bind(to: errorLabel.rx.isHidden)
+        .disposed(by: bag)
+        
+        viewModel.resultRelay.map { result -> String? in
+            switch result {
+            case .success: return nil
+            case .error(let error): return error.localizedDescription
+            }
+        }.bind(to: errorLabel.rx.text)
+        .disposed(by: bag)
     }
 }
 
