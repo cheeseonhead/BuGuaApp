280811b187cb1b0aef609d752b0c4c24cd38e92d
Jeff Woo <cheeseonhead@gmail.com>
2018-09-11 22:47:08 -0400

    Added some layout code for the sixlabelviews

--- BuGuaApp/GuaXiangViewLayout.swift
+++ BuGuaApp/GuaXiangViewLayout.swift
@@ -17,4 +17,18 @@ class GuaXiangViewLayout {
             make.centerY.equalTo(topLiuQin.snp.centerY)
         }
     }
+    
+    static func verticalAlignSixLabelView(_ sixLabelView: SixLabelView, shiYingYaoView: ShiYingYaoView) {
+        zip(sixLabelView.labels, shiYingYaoView.yaoViews).forEach { label, yao in
+            label.snp.makeConstraints { make in
+                make.bottom.equalTo(yao)
+            }
+        }
+        
+        zip(sixLabelView.labels, shiYingYaoView.shiYingLabels).forEach { label, shiYingLabel in
+            label.snp.makeConstraints { make in
+                make.top.equalTo(shiYingLabel)
+            }
+        }
+    }
 }
