5eac125b3564bbe10076ae2e57f5f2175857de35
Jeff Woo <cheeseonhead@gmail.com>
2018-09-11 23:16:27 -0400

    Added the liuQin labels

--- BuGuaApp/GuaXiangView.swift
+++ BuGuaApp/GuaXiangView.swift
@@ -17,6 +17,7 @@ class GuaXiangView: UIView {
     // MARK: - Views
     var shiYingYaoView: ShiYingYaoView!
     var diZhiView: SixLabelView!
+    var liuQinView: SixLabelView!
 
     // MARK: - Properties
     let guaXiangRelay = PublishRelay<LiuYaoGuaXiang>()
@@ -54,44 +55,57 @@ private extension GuaXiangView {
 
     func addViews() {
         setupShiYingYaoView()
-        setupDiZhiLabels()
+        setupDiZhiView()
+        setupLiuQinView()
     }
 
     func createConstraints() {
-        diZhiView.snp.makeConstraints { make in
-            make.leading.equalTo(shiYingYaoView.snp.trailing).offset(16)
-        }
-        GuaXiangViewLayout.verticalAlignSixLabelView(diZhiView, shiYingYaoView: shiYingYaoView)
-    }
-
-    func bindings() {
-        guaXiangRelay.bind(to: shiYingYaoView.guaXiangRelay)
-            .disposed(by: bag)
-        guaXiangRelay.map { $0.originalGua.yaoZhi.map { $0.character } }
-            .bind(to: diZhiView.dataRelay)
-            .disposed(by: bag)
-    }
-}
-
-// MARK: - Shi Ying Yao View
-private extension GuaXiangView {
-    func setupShiYingYaoView() {
-        shiYingYaoView = ShiYingYaoView(frame: .zero)
-        
-        addSubview(shiYingYaoView)
-        
         shiYingYaoView.snp.makeConstraints { make in
             make.centerX.equalTo(safeAreaLayoutGuide)
             make.bottom.equalTo(safeAreaLayoutGuide).offset(-8)
         }
+        
+        diZhiView.snp.makeConstraints { make in
+            make.leading.equalTo(shiYingYaoView.snp.trailing).offset(16)
+        }
+        GuaXiangViewLayout.verticalAlignSixLabelView(diZhiView, shiYingYaoView: shiYingYaoView)
+        
+        liuQinView.snp.makeConstraints { $0.trailing.equalTo(shiYingYaoView.snp.leading).offset(-16) }
+        GuaXiangViewLayout.verticalAlignSixLabelView(liuQinView, shiYingYaoView: shiYingYaoView)
+    }
+
+    func bindings() {
     }
 }
 
-// MARK: - DiZhi Labels
+// MARK: - Setup Views
 private extension GuaXiangView {
-    func setupDiZhiLabels() {
+    func setupShiYingYaoView() {
+        shiYingYaoView = ShiYingYaoView(frame: .zero)
+        
+        guaXiangRelay.bind(to: shiYingYaoView.guaXiangRelay)
+            .disposed(by: bag)
+        
+        addSubview(shiYingYaoView)
+    }
+    
+    func setupDiZhiView() {
         diZhiView = SixLabelView(frame: .zero)
         
+        guaXiangRelay.map { $0.originalGua.yaoZhi.map { $0.character } }
+            .bind(to: diZhiView.dataRelay)
+            .disposed(by: bag)
+        
         addSubview(diZhiView)
+    }
+    
+    func setupLiuQinView() {
+        liuQinView = SixLabelView(frame: .zero)
+
+        guaXiangRelay.map { $0.originalGua.liuQin.map { $0.character.vertical } }
+            .bind(to: liuQinView.dataRelay)
+            .disposed(by: bag)
+        
+        addSubview(liuQinView)
     }
 }
