e8112ce1dc4fcd4570851853d5ec5f69af214ed8
Jeffrey Wu <mengyang.developer@gmail.com>
2018-09-16 18:08:39 -0400

    Added coordinator model

---
+++ BuGuaApp/GuaXiangInputCoordinatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  GuaXiangInputCoordinatorModel.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-09-16.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
