fdc0d749ace899838be11fbb65c40b164c73f7b7
Jeffrey Wu <mengyang.developer@gmail.com>
2018-09-16 18:08:50 -0400

    Added some mothods to the model

--- BuGuaApp/GuaXiangInputCoordinatorModel.swift
+++ BuGuaApp/GuaXiangInputCoordinatorModel.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2018 Jeffrey Wu. All rights reserved.
 //
 
+import BuGuaKit
 import Foundation
+
+class GuaXiangInputCoordinatorModel {
+
+    // MARK: - Private properties
+    private let builder = LiuYaoGuaXiangBuilder()
+
+    func setLiuYao(_ liuYao: [YaoType]) {
+        builder.setLiuYao(liuYao)
+    }
+
+    func setDateGanZhi(_ ganZhi: DateGanZhi) {
+        builder.withDateGanZhi(ganZhi)
+    }
+}
+
+extension AppFactory {
+    func makeGuaXiangInputCoordinatorModel() -> GuaXiangInputCoordinatorModel {
+        return GuaXiangInputCoordinatorModel()
+    }
+}
