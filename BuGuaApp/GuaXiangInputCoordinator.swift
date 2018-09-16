5343b93551b7a2707ef1d71e801a16cc1579ad0c
Jeffrey Wu <jwu@touchbistro.com>
2018-09-16 18:09:05 -0400

    Request model when creating coordinator.

--- BuGuaApp/GuaXiangInputCoordinator.swift
+++ BuGuaApp/GuaXiangInputCoordinator.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Jeffrey Wu. All rights reserved.
 //
 
+import BuGuaKit
 import Foundation
 import RxSwift
 import RxCocoa
@@ -22,16 +23,17 @@ class GuaXiangInputCoordinator: Coordinator {
     private var navigationController: UINavigationController!
     private let didStartRelay = PublishRelay<UIViewController>()
     private let factory: AppFactory
+    private let model: GuaXiangInputCoordinatorModel
 
     // MARK: - Init
-    init(factory: AppFactory) {
+    init(factory: AppFactory, model: GuaXiangInputCoordinatorModel) {
         self.factory = factory
+        self.model = model
     }
 
     // MARK: - Lifecycle
     func start() {
-        let vm = factory.makeInputViewModel()
-        let vc = factory.makeInputViewController(viewModel: vm)
+        let vc = getInputViewController()
 
         navigationController = UINavigationController(rootViewController: vc)
 
@@ -39,8 +41,19 @@ class GuaXiangInputCoordinator: Coordinator {
     }
 }
 
+private extension GuaXiangInputCoordinator {
+    func getInputViewController() -> InputViewController {
+        let vm = factory.makeInputViewModel()
+        let vc = factory.makeInputViewController(viewModel: vm)
+
+//        vm.yaoTypeSignal.emit
+
+        return vc
+    }
+}
+
 extension AppFactory {
-    func makeGuaXiangInputCoordinator() -> GuaXiangInputCoordinator {
-        return GuaXiangInputCoordinator(factory: self)
+    func makeGuaXiangInputCoordinator(model: GuaXiangInputCoordinatorModel) -> GuaXiangInputCoordinator {
+        return GuaXiangInputCoordinator(factory: self, model: model)
     }
 }
