0163a4bc0f260bb070ca77c1f6a7db47ce9287d0
Jeffrey Wu <mengyang.developer@gmail.com>
2018-09-16 20:40:58 -0400

    Added files for the DateGanZhi scene

---
+++ BuGuaApp/DateGanZhiViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DateGanZhiViewModel.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-09-16.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+
+class DateGanZhiViewModel {
+    
+}
