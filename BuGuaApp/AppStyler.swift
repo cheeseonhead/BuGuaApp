a22cebd65ea18468996c5fe37b244bca3031c635
Jeffrey Wu <mengyang.developer@gmail.com>
2018-09-19 17:27:41 -0400

    Added a class called AppStyler

---
+++ BuGuaApp/AppStyler.swift
@@ -0,0 +1,21 @@
+//
+//  AppStyler.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-09-19.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppStyler {
+
+    let bodyColor = UIColor.spaceGrey
+
+    // MARK: - Labels
+    func navigationTitle(_ label: UILabel) {
+        label.textColor = bodyColor
+        label.font = .title2
+    }
+}
