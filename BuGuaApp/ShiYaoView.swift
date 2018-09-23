a8417f631c67ad9e82d6f8d8247a2bc636e53b5d
Jeffrey Wu <mengyang.developer@gmail.com>
2018-09-22 21:03:04 -0400

    Created the ShiYaoVIew

--- BuGuaApp/ShiYaoView.swift
+++ BuGuaApp/ShiYaoView.swift
@@ -6,4 +6,94 @@
 //  Copyright © 2018 Jeffrey Wu. All rights reserved.
 //
 
+import BuGuaKit
 import Foundation
+import RxCocoa
+import RxSwift
+import RxSwiftExt
+import UIKit
+
+private enum Style {
+    static let labelFont = UIFont.scaled(.body3Medium)
+
+    static let yaoViewSize = CGSize(width: 56, height: 36)
+}
+
+class ShiYaoView: UIView {
+
+    enum ShiYingType {
+        case shi, ying, none
+
+        static let `default` = ShiYingType.shi
+
+        var character: String {
+            switch self {
+            case .shi: return NSLocalizedString("世", comment: "")
+            case .ying: return NSLocalizedString("應", comment: "")
+            case .none: return " "
+            }
+        }
+    }
+
+    // MARK: - Views
+    let label = UILabel(frame: .zero)
+    let yaoView = YaoView(frame: .zero)
+
+    var stackView: UIStackView!
+
+    // MARK: - Input
+    let bag = DisposeBag()
+    let shiYingTypeRelay = BehaviorRelay(value: ShiYingType.default)
+    let yaoTypeRelay = BehaviorRelay(value: YaoType.oldYin)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        setup()
+    }
+}
+
+// MARK: - Setup
+private extension ShiYaoView {
+    func setup() {
+        stackView = UIStackView(arrangedSubviews: [label, yaoView])
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+
+        addSubview(stackView)
+
+        constarints()
+        bindings()
+        styling()
+    }
+
+    func constarints() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        yaoView.snp.makeConstraints { (make) in
+            make.size.equalTo(Style.yaoViewSize)
+        }
+    }
+
+    func bindings() {
+        shiYingTypeRelay.mapAt(\.character)
+            .bind(to: label.rx.text)
+            .disposed(by: bag)
+
+        yaoTypeRelay.bind(to: yaoView.yaoRelay)
+            .disposed(by: bag)
+    }
+
+    func styling() {
+        label.font = Style.labelFont
+    }
+}
