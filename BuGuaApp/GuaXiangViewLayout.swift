554d70be2db0ff469bb2b146168e7c2691fdace2
Jeffrey Wu <mengyang.developer@gmail.com>
2018-09-22 23:21:20 -0400

    Added a call to layout the header labels

--- BuGuaApp/GuaXiangViewLayout.swift
+++ BuGuaApp/GuaXiangViewLayout.swift
@@ -11,50 +11,10 @@ import SnapKit
 import UIKit
 
 class GuaXiangViewLayout {
-    static func layoutGuaGong(_ label: UILabel, topLiuQin: UILabel) {
-        label.snp.makeConstraints { make in
-            make.trailing.equalTo(topLiuQin.snp.leading).offset(-24)
-            make.centerY.equalTo(topLiuQin.snp.centerY)
-        }
-    }
-    
-    static func verticalAlignSixLabelView(_ sixLabelView: SixLabelView, shiYingYaoView: ShiYingYaoView) {
-        zip(sixLabelView.labels, shiYingYaoView.yaoViews).forEach { label, yao in
-            label.snp.makeConstraints { make in
-                make.bottom.equalTo(yao)
-            }
-        }
-        
-        zip(sixLabelView.labels, shiYingYaoView.shiYingLabels).forEach { label, shiYingLabel in
-            label.snp.makeConstraints { make in
-                make.top.equalTo(shiYingLabel)
-            }
-        }
-    }
-    
-    static func alignHeader(_ headerView: HeaderLabelView, columns: [UIView]) {
-        zip(headerView.headerLabels, columns).forEach { header, column in
+    static func alignHeader(_ headerView: HeaderLabelView, firstRow rowView: GuaXiangRow) {
+        zip(headerView.headerLabels, rowView.viewStack).forEach { header, column in
             header.snp.makeConstraints({ make in
                 make.centerX.equalTo(column)
-            })
-        }
-    }
-    
-    static func alignHorizontalDividers(_ dividerView: HorizontalDividersView, shiYingYaoView: ShiYingYaoView, headerView: HeaderLabelView) {
-        headerView.snp.makeConstraints { make in
-            make.top.equalTo(dividerView.headers.top.snp.bottom)
-            make.bottom.equalTo(dividerView.headers.bottom.snp.top)
-        }
-        
-        zip(shiYingYaoView.yaoViews.lazy.reversed(), dividerView.belowYao).forEach { yaoView, belowYao in
-            belowYao.snp.makeConstraints({ make in
-                make.top.equalTo(yaoView.snp.bottom)
-            })
-        }
-        
-        zip(shiYingYaoView.shiYingLabels.lazy.reversed().suffix(5), dividerView.belowYao.prefix(5)).forEach { shiYingLabel, aboveLabel in
-            aboveLabel.snp.makeConstraints({ make in
-                make.bottom.equalTo(shiYingLabel.snp.top)
             })
         }
     }
