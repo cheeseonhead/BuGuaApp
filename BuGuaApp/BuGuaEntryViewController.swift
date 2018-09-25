2763a530c2d7e8975caef8c6c3badc269a3c7b8f
Jeff Woo <cheeseonhead@gmail.com>
2018-09-24 23:17:49 -0400

    Made the view show guaxiang view controller

--- BuGuaApp/BuGuaEntryViewController.swift
+++ BuGuaApp/BuGuaEntryViewController.swift
@@ -14,43 +14,72 @@ import UIKit
 
 class BuGuaEntryViewController: UIViewController {
     // MARK: - Views
-    var inputButton: UIBarButtonItem!
+    let inputButton = UIBarButtonItem(title: NSLocalizedString("輸入", comment: ""), style: .plain, target: nil, action: nil)
+    
+    // MARK: - View Controllers
+    var guaXiangVC: GuaXiangViewController!
     
     // MARK: - Rx
     let bag = DisposeBag()
     
     // MARK: - Properties
+    let factory: AppFactory
     let viewModel: BuGuaEntryViewModel
     
     // MARK: - Inits
-    init(viewModel: BuGuaEntryViewModel) {
+    init(factory: AppFactory, viewModel: BuGuaEntryViewModel) {
+        self.factory = factory
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        
         createViews()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        view = BackgroundView(frame: .zero)
+        
+        let viewModel = factory.makeGuaXiangViewModel()
+        guaXiangVC = factory.makeGuaXiangViewController(viewModel: viewModel)
+        
+        add(guaXiangVC)
+        
+        setup()
+    }
 }
 
 // MARK: - Setup
 private extension BuGuaEntryViewController {
+    func setup() {
+        constraints()
+        bindings()
+        styling()
+    }
+    
     func createViews() {
-        view = BackgroundView(frame: .zero)
-        inputButton = UIBarButtonItem(title: NSLocalizedString("輸入", comment: ""), style: .plain, target: nil, action: nil)
         navigationItem.rightBarButtonItem = inputButton
+    }
+    
+    func constraints() {
+        guaXiangVC.view.snp.makeConstraints { make in
+            make.trailing.equalTo(view.snp.centerX)
+            make.centerY.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     func styling() {
     }
     
     func bindings() {
+        
     }
 }
 
 extension AppFactory {
     func makeBuGuaEntryViewController(viewModel: BuGuaEntryViewModel) -> BuGuaEntryViewController {
-        return BuGuaEntryViewController(viewModel: viewModel)
+        return BuGuaEntryViewController(factory: self, viewModel: viewModel)
     }
 }
