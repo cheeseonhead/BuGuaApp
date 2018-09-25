39fd5276394c4aa84e9bd53e59287cea089fcbba
Jeff Woo <cheeseonhead@gmail.com>
2018-09-24 22:18:54 -0400

    Added the BuGuaEntry Scene into the app

---
+++ BuGuaApp/BuGuaEntryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BuGuaEntryViewController.swift
+//  BuGuaApp
+//
+//  Created by Cheese Onhead on 2018-09-24.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BuGuaEntryViewController: UIViewController {
+    
+}
---
+++ BuGuaApp/BuGuaEntryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BuGuaEntryViewModel.swift
+//  BuGuaApp
+//
+//  Created by Cheese Onhead on 2018-09-24.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+
+class BuGuaEntryViewModel {
+    
+}
