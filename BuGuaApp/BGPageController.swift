90ab51b1dc3b93545d520cd3da51266378919a60
Jeffrey Wu <mengyang.developer@gmail.com>
2018-09-26 23:33:52 -0400

    #55 | Added a BGPageController class

---
+++ BuGuaApp/BGPageController.swift
@@ -0,0 +1,27 @@
+//
+//  BGPageController.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-09-26.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import UIKit
+
+class BGPageController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
