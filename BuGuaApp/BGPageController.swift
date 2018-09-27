07f41f2a7689f1404def5a8cddff1f13feb94faf
Jeffrey Wu <jwu@touchbistro.com>
2018-09-27 13:38:49 -0400

    #55 | Added a scrollVIew

--- BuGuaApp/BGPageController.swift
+++ BuGuaApp/BGPageController.swift
@@ -8,20 +8,61 @@
 
 import UIKit
 
+private enum Style {
+    static let contentSpacing = BGStyle.standardMargin / 2
+    static let horizontalContentInset = contentSpacing
+}
+
 class BGPageController: UIViewController {
+
+    // MARK: - Config
+
+    var minSize = CGSize.zero {
+        didSet { view.setNeedsLayout() }
+    }
+
+    var contentRatio = CGFloat(1) {
+        didSet { view.setNeedsLayout() }
+    }
+
+    // MARK: - Views
+
+    let scrollView = UIScrollView(frame: .zero)
+
+    // MARK: - Properties
+
+    let viewControllers: [UIViewController]
+
+    init(viewControllers: [UIViewController]) {
+        self.viewControllers = viewControllers
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder _: NSCoder) {
+        viewControllers = []
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setup()
     }
 
-    /*
-     // MARK: - Navigation
+    override func viewDidLayoutSubviews() {
+    }
+}
 
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+// MARK: - Setup
+
+private extension BGPageController {
+    func setup() {
+        view.addSubview(scrollView)
+
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaInsets)
+        }
+    }
 }
