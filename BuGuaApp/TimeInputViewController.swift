529099b338fec0c14b7fc7fd565c52c941f8f2f5
Jeffrey Wu <jwu@touchbistro.com>
2018-09-30 16:46:54 -0400

    #67 | Added the files required for timeinputviewcontroller

---
+++ BuGuaApp/TimeInputViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TimeInputViewController.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-09-30.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+import RxSwiftExt
+import UIKit
+
+class TimeInputViewController: UIViewController {
+
+    // MARK: - Views
+    @IBOutlet weak var timePicker: UIDatePicker!
+
+    
+}
