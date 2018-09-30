7ff707ecdcf1c027c03781abecc27b636745e714
Jeffrey Wu <jwu@touchbistro.com>
2018-09-30 17:05:56 -0400

    #67 | Added Time GanZhi scene files

---
+++ BuGuaApp/TimeGanZhiViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TimeGanZhiViewModel.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-09-30.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+
+class TimeGanZhiViewModel {
+    
+}
