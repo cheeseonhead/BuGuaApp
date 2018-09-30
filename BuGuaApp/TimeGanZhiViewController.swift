86ecfdf08dc2998589676c24855407dd4ffe4e0f
Jeffrey Wu <mengyang.developer@gmail.com>
2018-09-30 17:05:56 -0400

    #67 | Added Time GanZhi scene files

---
+++ BuGuaApp/TimeGanZhiViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimeGanZhiViewController.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-09-30.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import UIKit
+
+class TimeGanZhiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
