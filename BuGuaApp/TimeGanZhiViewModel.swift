45a62eeb151aaff10bb46df72fd1d5280b9d5a1f
Jeffrey Wu <jwu@touchbistro.com>
2018-09-30 17:16:29 -0400

    #67 | Some template things

--- BuGuaApp/TimeGanZhiViewModel.swift
+++ BuGuaApp/TimeGanZhiViewModel.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 class TimeGanZhiViewModel {
-    
+
+}
+
+extension AppFactory {
+    func makeTimeGanZhiViewModel() -> TimeGanZhiViewModel {
+        return TimeGanZhiViewModel()
+    }
 }
