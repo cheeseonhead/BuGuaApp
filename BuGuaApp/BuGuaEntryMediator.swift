224f5352920cb4d0abaff3c8f55781349abcba84
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-03 18:36:59 -0400

    #81 | Added a mediator class

---
+++ BuGuaApp/BuGuaEntryMediator.swift
@@ -0,0 +1,17 @@
+//
+//  BuGuaEntryMediator.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-03.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+
+class BuGuaEntryMediator {
+    let storageManager: StorageManager
+
+    init(storageManager: StorageManager) {
+        self.storageManager = storageManager
+    }
+}
