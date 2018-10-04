14936324a9b5fca75615fd7b0357ff023ebae892
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-03 20:15:23 -0400

    #81 | Added some preliminary protocols and mediator

--- BuGuaApp/BuGuaEntryMediator.swift
+++ BuGuaApp/BuGuaEntryMediator.swift
@@ -6,12 +6,76 @@
 //  Copyright © 2018 Jeffrey Wu. All rights reserved.
 //
 
+import BuGuaKit
+import CoreData
 import Foundation
+import RxCocoa
+import RxSwift
+import RxSwiftExt
 
-class BuGuaEntryMediator {
+protocol Migratable: Equatable {
+    associatedtype Counterpart
+    associatedtype Context
+
+    static func build(from: Counterpart) -> Self?
+    func export(toContext:Context) throws -> Counterpart
+//    static func delete(counterpart:Counterpart, fromContext:Context) throws
+}
+
+protocol Mediator {
+    associatedtype ManagedObject
+    associatedtype StructElement: Migratable where StructElement.Counterpart == ManagedObject
+
+    init(structElement: StructElement, storageManager: StorageManager) throws
+    init(object: ManagedObject, storageManager: StorageManager)
+
+    var input: PublishRelay<StructElement> { get }
+    var output: Driver<StructElement> { get }
+}
+
+extension BuGuaEntry: Migratable {
+    typealias Counterpart = BuGuaEntryObject
+    typealias Context = NSManagedObjectContext
+
+
+    static func build(from: BuGuaEntryObject) -> BuGuaEntry? {
+        return BuGuaEntry.default
+    }
+
+    func export(toContext: NSManagedObjectContext) throws -> BuGuaEntryObject {
+        return BuGuaEntryObject(context: toContext)
+    }
+
+}
+
+class BuGuaEntryMediator: Mediator {
+    typealias StructElement = BuGuaEntry
+
+    let input = PublishRelay<BuGuaEntry>()
+    private (set) lazy var output = buGuaEntryOutput.asDriver()
+
+    let buGuaEntryObject: BuGuaEntryObject
     let storageManager: StorageManager
 
-    init(storageManager: StorageManager) {
+    private let buGuaEntryOutput = BehaviorRelay<BuGuaEntry>(value: .default)
+
+    required init(structElement: BuGuaEntry, storageManager: StorageManager) throws {
+        buGuaEntryObject = try structElement.export(toContext: storageManager.context)
         self.storageManager = storageManager
     }
+
+    required init(object: BuGuaEntryObject, storageManager: StorageManager) {
+        self.buGuaEntryObject = object
+        self.storageManager = storageManager
+    }
+}
+
+extension BuGuaEntry {
+    static let `default` = {
+        BuGuaEntryBuilder()
+            .setDate(.zero)
+            .setTime(.zero)
+            .setGuaXiang(.default)
+            .build()
+    }()
 }
