c2a11f4b4e6cd8b2d77c650bb81b675bcb5cdd6f
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-04 19:54:39 -0400

    #81 | Added files for the NSObject extensions

---
+++ BuGuaApp/BuGuaEntryExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  BuGuaEntryExtensions.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-04.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
---
+++ BuGuaApp/GregorianDateExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  GregorianDateExtensions.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-04.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
