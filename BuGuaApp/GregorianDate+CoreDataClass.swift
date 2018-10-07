937210f6309f27778fc135af6c400e8ffb3c3a03
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-07 17:58:17 -0400

    #83 | Generated the files

---
+++ BuGuaApp/GregorianDate+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  GregorianDate+CoreDataClass.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-07.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(GregorianDate)
+public class GregorianDate: NSManagedObject {
+
+}
