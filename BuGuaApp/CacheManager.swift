8a6884353b6d1f1a90a9caa4e77bf97224189ddf
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-07 18:04:11 -0400

    #83 | Added three manager classes and files

---
+++ BuGuaApp/CacheManager.swift
@@ -0,0 +1,13 @@
+//
+//  CacheManager.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-07.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+
+class CacheManager {
+
+}
