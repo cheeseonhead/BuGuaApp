42568deaaa880d53bd85d621bf920501b7bc1e6d
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-07 18:49:42 -0400

    #83 | Copied the cacheUpdate method

--- BuGuaApp/CacheManager.swift
+++ BuGuaApp/CacheManager.swift
@@ -11,8 +11,23 @@ import Foundation
 
 class CacheManager {
     private let context: NSManagedObjectContext
+    private let dateGenerator: () -> NSDate
 
-    init(context: NSManagedObjectContext) {
+    init(context: NSManagedObjectContext, dateGenerator: @escaping () -> NSDate) {
         self.context = context
+        self.dateGenerator = dateGenerator
+    }
+
+    func cacheUpdate(ids: [NSManagedObjectID]) {
+        let uris = ids.map { $0.uriRepresentation() }
+
+        context.perform { [unowned self] in
+            uris.forEach {
+                let cacheObject = CacheRecord(context: self.context)
+                cacheObject.managedObjectId = $0
+                cacheObject.nextTryTimestamp = self.dateGenerator()
+            }
+            try! self.context.save()
+        }
     }
 }
