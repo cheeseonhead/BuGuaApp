3c2443081dc5750644b46663f114fef668a67fae
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-07 18:21:36 -0400

    #83 | Added three essential protocols

---
+++ BuGuaApp/ManagedConvertable.swift
@@ -0,0 +1,20 @@
+//
+//  ManagedConvertable.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-05.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+
+import Foundation
+import CoreData
+protocol ManagedConvertable {
+    associatedtype Context
+    associatedtype ObjectType: ImmutableConvertable where ObjectType.Context == Context
+    func managedObject(inConext context: NSManagedObjectContext) -> ObjectType
+}
+extension ManagedConvertable where Self == ObjectType.ImmutableType {
+    func managedObject(inConext context: Context) -> ObjectType {
+        return ObjectType.build(from: self, inContext: context)
+    }
+}
---
+++ BuGuaApp/Mediator.swift
@@ -0,0 +1,19 @@
+//
+//  Mediator.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-06.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+import Foundation
+import RxCocoa
+import RxSwift
+import RxSwiftExt
+
+protocol Mediator {
+    associatedtype ManagedObject: ImmutableConvertable
+    init(immutable: ManagedObject.ImmutableType, storageManager: StorageManager)
+    init(object: ManagedObject, storageManager: StorageManager)
+    var input: PublishRelay<ManagedObject.ImmutableType> { get }
+    var output: Driver<ManagedObject.ImmutableType> { get }
+}
