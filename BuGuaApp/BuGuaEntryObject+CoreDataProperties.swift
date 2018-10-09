0b3696cd6cbc3a55d94aff490d03177a2ee0ef77
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-08 20:38:36 -0400

    #83 | Updated the models to hold reference to ckrecord

--- BuGuaApp/BuGuaEntryObject+CoreDataProperties.swift
+++ BuGuaApp/BuGuaEntryObject+CoreDataProperties.swift
@@ -2,21 +2,19 @@
 //  BuGuaEntryObject+CoreDataProperties.swift
 //  BuGuaApp
 //
-//  Created by Jeffrey Wu on 2018-10-07.
+//  Created by Jeffrey Wu on 2018-10-08.
 //  Copyright © 2018 Jeffrey Wu. All rights reserved.
 //
 //
 
-import Foundation
 import CoreData
-
+import Foundation
 
 extension BuGuaEntryObject {
-
     @nonobjc public class func fetchRequest() -> NSFetchRequest<BuGuaEntryObject> {
         return NSFetchRequest<BuGuaEntryObject>(entityName: "BuGuaEntryObject")
     }
 
+    @NSManaged public var ckData: NSData?
     @NSManaged public var date: GregorianDateObject
-
 }
--- BuGuaApp/GregorianDateObject+CoreDataProperties.swift
+++ BuGuaApp/GregorianDateObject+CoreDataProperties.swift
@@ -2,24 +2,22 @@
 //  GregorianDateObject+CoreDataProperties.swift
 //  BuGuaApp
 //
-//  Created by Jeffrey Wu on 2018-10-07.
+//  Created by Jeffrey Wu on 2018-10-08.
 //  Copyright © 2018 Jeffrey Wu. All rights reserved.
 //
 //
 
-import Foundation
 import CoreData
-
+import Foundation
 
 extension GregorianDateObject {
-
     @nonobjc public class func fetchRequest() -> NSFetchRequest<GregorianDateObject> {
         return NSFetchRequest<GregorianDateObject>(entityName: "GregorianDateObject")
     }
 
-    @NSManaged public var year: Int64
-    @NSManaged public var month: Int64
     @NSManaged public var day: Int64
+    @NSManaged public var month: Int64
+    @NSManaged public var year: Int64
+    @NSManaged public var ckData: NSData?
     @NSManaged public var buGuaEntry: BuGuaEntryObject
-
 }
