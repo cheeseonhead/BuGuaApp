90ac98fdefa53471355920347b426e58b8c5837a
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-12 17:22:32 -0400

    #86 | Added some integer conversion

--- BuGuaApp/Int+Conversion.swift
+++ BuGuaApp/Int+Conversion.swift
@@ -8,21 +8,23 @@
 
 import Foundation
 
+// MARK: - String
+
 extension Int {
     enum Error: LocalizedError {
         case stringEmpty
         case notValidInteger(String)
-        
+
         var errorDescription: String? {
             switch self {
             case .stringEmpty:
                 return NSLocalizedString("空字串不是整數", comment: "")
-            case .notValidInteger(let str):
+            case let .notValidInteger(str):
                 return String(format: NSLocalizedString("\"%@\"並非整數", comment: ""), str)
             }
         }
     }
-    
+
     init?(str: String?) throws {
         guard let str = str, !str.isEmpty else {
             return nil
@@ -31,7 +33,21 @@ extension Int {
         guard let integer = Int(str) else {
             throw Error.notValidInteger(str)
         }
-        
+
         self = integer
+    }
+}
+
+// MARK: - Int64
+
+extension Int {
+    var int64: Int64 {
+        return Int64(self)
+    }
+}
+
+extension Int64 {
+    var int: Int {
+        return Int(self)
     }
 }
