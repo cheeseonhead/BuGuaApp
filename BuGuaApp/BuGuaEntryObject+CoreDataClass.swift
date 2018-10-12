b273236343744ba51edca87afc59ae57c3dc6488
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-12 19:40:25 -0400

    #88 | Added the gregorianDate and Buguaentry models

---
+++ BuGuaApp/BuGuaEntryObject+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  BuGuaEntryObject+CoreDataClass.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-12.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+//
+
+import CoreData
+import Foundation
+
+@objc(BuGuaEntryObject)
+public class BuGuaEntryObject: NSManagedObject {}
---
+++ BuGuaApp/GregorianDateObject+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  GregorianDateObject+CoreDataProperties.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-12.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+//
+
+import CoreData
+import Foundation
+
+extension GregorianDateObject {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<GregorianDateObject> {
+        return NSFetchRequest<GregorianDateObject>(entityName: "GregorianDateObject")
+    }
+
+    @NSManaged public var recordName: String?
+    @NSManaged public var recordId: NSData?
+    @NSManaged public var year: Int64
+    @NSManaged public var month: Int64
+    @NSManaged public var day: Int64
+    @NSManaged public var entry: BuGuaEntryObject?
+}
