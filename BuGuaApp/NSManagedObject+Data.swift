9a3c5f726f019df4d373a0a07b9c69967df8f2e6
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-12 19:46:19 -0400

    #88 | #88 | Added the extension for entityName

---
+++ BuGuaApp/NSManagedObject+Data.swift
@@ -0,0 +1,15 @@
+//
+//  NSManagedObject+Data.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-08.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+import CoreData
+import Foundation
+
+extension NSManagedObject {
+    static var entityName: String {
+        return String(describing: self)
+    }
+}
