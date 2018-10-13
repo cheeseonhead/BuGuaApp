f03cefb709397fd56c6f6b9286b576731f7319ca
Cheeseonhead <mengyang.developer@gmail.com>
2018-10-12 20:33:17 -0400

    #90 | Created entity CacheRecord

---
+++ BuGuaApp/CacheRecord+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  CacheRecord+CoreDataClass.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-12.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+//
+
+import CoreData
+import Foundation
+
+@objc(CacheRecord)
+public class CacheRecord: NSManagedObject {}
---
+++ BuGuaApp/CacheRecord+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  CacheRecord+CoreDataProperties.swift
+//  BuGuaApp
+//
+//  Created by Jeffrey Wu on 2018-10-12.
+//  Copyright © 2018 Jeffrey Wu. All rights reserved.
+//
+//
+
+import CoreData
+import Foundation
+
+extension CacheRecord {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CacheRecord> {
+        return NSFetchRequest<CacheRecord>(entityName: "CacheRecord")
+    }
+
+    @NSManaged public var managedObjectId: URL?
+    @NSManaged public var nextTryTimestamp: NSDate
+    @NSManaged public var recordID: NSData?
+}
