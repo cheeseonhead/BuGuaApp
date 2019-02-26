a5b59bdccfc01688978e95efca78c0fa787e766d
Cheeseonhead <mengyang.developer@gmail.com>
2019-02-26 17:40:56 -0500

    #99 | Added a id function to BuGuaKit, and renamed a function

---
+++ BuGuaKit/Sources/BuGuaKit/UniversalFunctions.swift
@@ -0,0 +1,13 @@
+//
+//  UniversalFunctions.swift
+//  BuGuaKit
+//
+//  Created by Jeffrey Wu on 2019-02-26.
+//  Copyright © 2019 cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+public func id<A>(_ a: A) -> A {
+    return a
+}
